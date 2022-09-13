c4b888f623e46df8379eb481217a7987a728709c
Lucas Felipe <80704314+LFeli@users.noreply.github.com>
2022-09-13 12:02:09 -0300

    Update aplic.java

--- Exercicio-1/aplic.java
+++ Exercicio-1/aplic.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exercicio1 {
+public class aplic {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int qtdeVend,condPag;
