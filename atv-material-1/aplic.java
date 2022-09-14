7596359b1fd631c64f193dfad7df2a7684c2fa75
Lucas Felipe <80704314+LFeli@users.noreply.github.com>
2022-09-13 22:11:12 -0300

    Update aplic.java

--- atv-material-1/aplic.java
+++ atv-material-1/aplic.java
@@ -1,10 +1,12 @@
 import model.Circulo;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class aplic {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
         Circulo objCirculo = new Circulo();
         
         double raio;
@@ -13,8 +15,8 @@ public class aplic {
         raio = entrada.nextDouble();
         objCirculo.setRaio(raio);
         
-        System.out.println("A área do Circulo é: " + objCirculo.calcArea());
-        System.out.println("A medida do perimetro é: " + objCirculo.calcPerimentro());
+        System.out.println("A área do Circulo é: " + df.format(objCirculo.calcArea()));
+        System.out.println("A medida do perimetro é: " + df.format(objCirculo.calcPerimentro()));
         System.out.println("A medida do diametro é: " + objCirculo.calcDiametro());
     }
     
