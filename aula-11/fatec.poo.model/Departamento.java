1e171579ba0fcf7be61b0085289d8b6e93dceb4b
Lucas Felipe <lucasfelipemk@gmail.com>
2022-11-10 10:41:17 -0300

    aula-11 done

---
+++ aula-11/fatec.poo.model/Departamento.java
@@ -0,0 +1,39 @@
+package fatec.poo.model;
+
+import java.util.ArrayList;
+
+public class Departamento {
+    private String sigla;
+    private String nome;
+    private ArrayList<Funcionario> funcionarios;//multiplicidade 1..* 
+    
+    public Departamento(String sigla, String nome) {
+        this.sigla = sigla;
+        this.nome = nome;
+        funcionarios = new ArrayList<Funcionario>();
+    }
+
+    public String getSigla() { 
+        return sigla;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public void addFuncionario(Funcionario f){
+        funcionarios.add(f);
+    }
+    
+    public void listarFuncionarios(){
+        System.out.println("\n\nSigla: " + sigla);
+        System.out.println("Nome: " + nome);
+        System.out.println("Qtde. Funcionários: " + funcionarios.size());
+        System.out.println("\nRegistro\t\tNome\t\tCargo");
+        for (int x = 0; x < funcionarios.size(); x++){
+            System.out.print(funcionarios.get(x).getRegistro() + "\t\t");
+            System.out.print(funcionarios.get(x).getNome() + "\t\t");
+            System.out.println(funcionarios.get(x).getCargo());
+        }
+    }
+}
---
+++ aula-11/fatec.poo.model/FuncionarioHorista.java
@@ -0,0 +1,30 @@
+package fatec.poo.model;
+
+public class FuncionarioHorista extends Funcionario{
+    private double valHorTrab;
+    private int qtdeHorTrab;
+
+    public FuncionarioHorista(int registro,String nome,String dtAdmissao,double valHorTrab) {
+        super(registro, nome, dtAdmissao);
+        this.valHorTrab = valHorTrab;
+    }
+
+    public void setQtdeHorTrab(int qtdeHorTrab) {
+        this.qtdeHorTrab = qtdeHorTrab;
+    }
+
+    @Override
+    public double calcSalBruto() {
+       return(valHorTrab * qtdeHorTrab);
+    }
+    
+    public double calcGratificacao(){
+        return(0.075 * calcSalBruto());
+    }
+    
+    //Aplicação do polimorfismo
+    @Override
+    public double calcSalLiquido(){
+        return(super.calcSalLiquido() + calcGratificacao());
+    }
+}
